45fc830f83d1017e01d13d2835166758c1320456
David (Pololu) <dev-david@pololu.com>
2011-09-20 14:50:32 -0700

    led_strip app: reduced the period, increased the priority, and got it to produce signals good enough for an LED strip.

--- apps/led_strip_controller/led_strip_controller.c
+++ apps/led_strip_controller/led_strip_controller.c
@@ -20,18 +20,18 @@ void ledStripInit()
 
     // Set Timer 3 modulo mode (period is set by T3CC0)
     // Set Timer 3 Channel 1 (P1_4) to output compare mode.
-    T3CC0 = 48;       // Set the period
-    T3CC1 = 12;       // Set the duty cycle.
+    T3CC0 = 23;       // Set the period
+    T3CC1 = 11;       // Set the duty cycle.
     T3CCTL0 = 0b00000100;  // Enable the channel 0 compare, which triggers the DMA.
     T3CCTL1 = 0b00100100;  // T3CH1: Timer disabled, clear output on compare up, set on 0.
     T3CTL = 0b00010010;  // Start the timer with Prescaler 1:1, modulo mode (counts from 0 to T3CC0).
 
     for(i=0; i < sizeof(bitBuffer); i += 4)
     {
-        bitBuffer[i+0] = 11;
-        bitBuffer[i+1] = 47;
-        bitBuffer[i+2] = 11;
-        bitBuffer[i+3] = 47;
+        bitBuffer[i+0] = 8;
+        bitBuffer[i+1] = 15;
+        bitBuffer[i+2] = 8;
+        bitBuffer[i+3] = 15;
     }
 
     LED_DMA.DC6 = 19; // WORDSIZE = 0, TMODE = 0, TRIG = 19
@@ -42,7 +42,7 @@ void ledStripInit()
     LED_DMA.LENL = sizeof(bitBuffer);
     LED_DMA.VLEN_LENH = 0;
     LED_DMA.DC6 = 0b01000111; // WORSIZE = 0, TMODE = repeated single, TRIG = 7 (Timer 3 compare ch 0)
-    LED_DMA.DC7 = 0b01000000; // SRCINC = 1, DESTINC = 0, IRQMASK = 0, M8 = 0, PRIORITY = 0
+    LED_DMA.DC7 = 0b01000010; // SRCINC = 1, DESTINC = 0, IRQMASK = 0, M8 = 0, PRIORITY = 2 (High)
 
     DMAARM |= (1 << DMA_CHANNEL_LED);
 }
