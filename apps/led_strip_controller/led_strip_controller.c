ca7d7006c980ea55caec90f6153c8591fb6772b2
David (Pololu) <dev-david@pololu.com>
2011-09-20 14:47:19 -0700

    led_strip app:  Made it trigger off of Channel 0 compare, which happens at the end of every cycle.  Minimum duty cycle now is 11, and there is no maximum.

--- apps/led_strip_controller/led_strip_controller.c
+++ apps/led_strip_controller/led_strip_controller.c
@@ -7,6 +7,9 @@
 
 uint8 XDATA bitBuffer[96];
 
+// Experimentally, we found that duty cycles of 10 or lower are bad.
+// It must be because the DMA takes some time.
+
 void ledStripInit()
 {
     uint8 i;
@@ -19,15 +22,16 @@ void ledStripInit()
     // Set Timer 3 Channel 1 (P1_4) to output compare mode.
     T3CC0 = 48;       // Set the period
     T3CC1 = 12;       // Set the duty cycle.
+    T3CCTL0 = 0b00000100;  // Enable the channel 0 compare, which triggers the DMA.
     T3CCTL1 = 0b00100100;  // T3CH1: Timer disabled, clear output on compare up, set on 0.
     T3CTL = 0b00010010;  // Start the timer with Prescaler 1:1, modulo mode (counts from 0 to T3CC0).
 
     for(i=0; i < sizeof(bitBuffer); i += 4)
     {
-        bitBuffer[i+0] = 10;
-        bitBuffer[i+1] = 20;
-        bitBuffer[i+2] = 30;
-        bitBuffer[i+3] = 32;
+        bitBuffer[i+0] = 11;
+        bitBuffer[i+1] = 47;
+        bitBuffer[i+2] = 11;
+        bitBuffer[i+3] = 47;
     }
 
     LED_DMA.DC6 = 19; // WORDSIZE = 0, TMODE = 0, TRIG = 19
@@ -37,7 +41,7 @@ void ledStripInit()
     LED_DMA.DESTADDRL = XDATA_SFR_ADDRESS(T3CC1);
     LED_DMA.LENL = sizeof(bitBuffer);
     LED_DMA.VLEN_LENH = 0;
-    LED_DMA.DC6 = 0b01001000; // WORSIZE = 0, TMODE = repeated single, TRIG = 8 (Timer 3 compare ch 1)
+    LED_DMA.DC6 = 0b01000111; // WORSIZE = 0, TMODE = repeated single, TRIG = 7 (Timer 3 compare ch 0)
     LED_DMA.DC7 = 0b01000000; // SRCINC = 1, DESTINC = 0, IRQMASK = 0, M8 = 0, PRIORITY = 0
 
     DMAARM |= (1 << DMA_CHANNEL_LED);
