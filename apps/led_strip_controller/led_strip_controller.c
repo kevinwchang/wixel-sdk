a629ec3eac65872ddbf8d858831b097f4f94f575
Kevin (Pololu) <dev-kevin@pololu.com>
2013-10-29 23:47:42 -0700

    works with neopixel ring

--- apps/led_strip_controller/led_strip_controller.c
+++ apps/led_strip_controller/led_strip_controller.c
@@ -8,18 +8,19 @@ void updateBitBuffer(void);
 #define LED_DMA (dmaConfig._2)
 #define DMA_CHANNEL_LED 2
 
-#define INVERT // invert output to drive NMOS gate (level-shift to 5 V)
+//#define INVERT // invert output to drive NMOS gate (level-shift to 5 V)
 
-// These values are consistent with the datasheet, and also work in practice.
-// There is a rise time of about 80ns (two ticks) dues to the 1k pull-up resistor.
-#define PERIOD 25
-#define LED_BIT_L 12
-#define LED_BIT_H 18
+// units of 1/24 us
+#define PERIOD 30
+#define LED_BIT_L 10
+#define LED_BIT_H 19
 
-#define LED_DATA_BITS 768
-
-#define LED_COUNT  (LED_DATA_BITS/24)
 #define LED_BIT(v) ((v) ? LED_BIT_H : LED_BIT_L)
+
+#define LED_COUNT 16
+#define LED_DATA_BITS  (LED_COUNT*24)
+
+
 
 volatile uint8 XDATA bitBuffer[LED_DATA_BITS+2];
 
@@ -41,7 +42,7 @@ void ledStripInit()
 
     // Set Timer 3 modulo mode (period is set by T3CC0)
     // Set Timer 3 Channel 1 (P1_4) to output compare mode.
-    T3CC0 = PERIOD;       // Set the period
+    T3CC0 = PERIOD - 1;   // Set the period; we want the counter to reset to 0 on the tick after it reaches (PERIOD - 1)
     T3CC1 = 0;            // Set the duty cycle.
     T3CCTL0 = 0b00000100; // Enable the channel 0 compare, which triggers the DMA at the end of every period.
 #ifdef INVERT
@@ -57,10 +58,10 @@ void ledStripInit()
     LED_DMA.DESTADDRL = XDATA_SFR_ADDRESS(T3CC1);
     LED_DMA.LENL = (uint8)sizeof(bitBuffer);
     LED_DMA.VLEN_LENH = (uint8)(sizeof(bitBuffer) >> 8);
-    LED_DMA.DC6 = 0b00000111; // WORDSIZE = 0 (8-bit), TMODE = 0 (single), TRIG = 7 (Timer 3 compare ch 0)
+    LED_DMA.DC6 = 0b00000111; // WORDSIZE = 0 (8-bit), TMODE = 0 (Single), TRIG = 7 (Timer 3, compare, channel 0)
     LED_DMA.DC7 = 0b01000010; // SRCINC = 1, DESTINC = 0, IRQMASK = 0, M8 = 0, PRIORITY = 2 (High)
 
-    // We found that I priority of 1 (equal to the CPU) also works.
+    // We found that a priority of 1 (equal to the CPU) also works.
 
 }
 
@@ -74,24 +75,23 @@ void ledStripService()
         updateBitBuffer();
 
         // Start the transfer of data to the LED strip.
-        // It will finish in about LED_DATA_BITS * (PERIOD + 1) / 24 microseconds.
+        // It will finish in about LED_DATA_BITS * PERIOD / 24 microseconds.
         DMAARM |= (1 << DMA_CHANNEL_LED);
     }
 }
 
 void updateBitBuffer()
 {
-    uint16 time = getMs() >> 2;
+    uint8 time = (getMs() >> 5) & 0xFF;
     uint8 i, j;
 
     for(i = 0; i < LED_COUNT; i++)
     {
-        time -= 8;
         for(j = 0; j < 8; j++)
         {
-            bitBuffer[1 + 24*i + j] = LED_BIT(time >> (7-j) & 1);
-            bitBuffer[1 + 24*i + 8 + j] = LED_BIT(~time >> (7-j) & 1);
-            bitBuffer[1 + 24*i + 16 + j] = LED_BIT(time >> (7-j) & 1);
+            bitBuffer[1 + 24*i + j] = LED_BIT(60 >> (7-j) & 1); // G
+            bitBuffer[1 + 24*i + 8 + j] = LED_BIT(20 >> (7-j) & 1); // R
+            bitBuffer[1 + 24*i + 16 + j] = LED_BIT(100 >> (7-j) & 1); // B
         }
     }
 }
@@ -102,13 +102,15 @@ void main()
     usbInit();
     ledStripInit();
 
+    ledStripService();
+    
     while(1)
     {
         usbComService();
         usbShowStatusWithGreenLed();
-        LED_YELLOW(getMs() >> 9 & 1);
+        //LED_YELLOW(getMs() >> 9 & 1);
 
         boardService();
-        ledStripService();
+        
     }
 }
